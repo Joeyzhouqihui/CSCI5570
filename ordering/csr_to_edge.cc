f8e05155909af1e46f87820968245d2bc65e9f05
Joeyzhouqihui <joeyzhouqihui@gmail.com>
2021-04-11 13:38:57 +0800

    dev

---
+++ ordering/csr_to_edge.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  ifstream file(argv[1], ios::in);
+  int vcnt, ecnt, vid1, vid2;
+  file>>vcnt>>ecnt;
+  vector<int> offsets(vcnt + 1, 0);
+  vector<int> edges(ecnt, 0);
+  for (int i=0; i<vcnt; i++) {
+    file>>offsets[i];
+  }
+  offsets[vcnt] = ecnt;
+  for (int i=0; i<ecnt; i++) {
+    file>>edges[i];
+  }
+  string ofilename = "edge";
+  ofstream ofile(ofilename.c_str(), ios::out);
+  for (int i=0; i<vcnt; i++) {
+    for (int j=offsets[i]; j<offsets[i+1]; j++) {
+      ofile<<i<<" "<<edges[j]<<"\n";
+    }
+  }
+  ofile.close();
+  return 0;
+}
---
+++ ordering/edge_to_csr.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  ifstream file(argv[1], ios::in);
+  map<int, vector<int> > graph;
+  ofstream ofile("csr", ios::out);
+  int vid1, vid2, max_vid = -1, edge_cnt = 0;
+  while (file>>vid1>>vid2) {
+    graph[vid1].push_back(vid2);
+    edge_cnt++;
+    if (max_vid < vid1) {
+      max_vid = vid1;
+    }
+    if (max_vid < vid2) {
+      max_vid = vid2;
+    }
+  }
+  ofile<<(max_vid + 1)<<'\n';
+  ofile<<edge_cnt<<'\n';
+  int cnt_off = 0;
+  for (int i=0; i<=max_vid; i++) {
+    ofile<<cnt_off<<'\n';
+    cnt_off += graph[i].size();
+  }
+  for (int i=0; i<=max_vid; i++) {
+    sort(graph[i].begin(), graph[i].end());
+    for (const auto &vid : graph[i]) {
+      ofile<<vid<<'\n';
+    }
+  }
+  ofile.close();
+  return 0;
+}
