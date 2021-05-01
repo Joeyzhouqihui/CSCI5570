b08e3f4eada165e616b35d9c1eda440c4b75cd16
Joeyzhouqihui <joeyzhouqihui@gmail.com>
2021-05-01 15:40:08 +0800

    dev

--- ordering/gorder/main.cpp
+++ ordering/gorder/main.cpp
@@ -27,38 +27,15 @@ int main(int argc, char* argv[]){
 	int i;
 	int W=5;
 	clock_t start, end;
-	string filename;
-
-	if(argc==1){
-		cout << "please provide parameter" << endl;
-		quit();
-	}
-
-	i=1;
-	while(i<argc){
-		if(strcmp("-w", argv[i])==0){
-			i++;
-			W=atoi(argv[i]);
-			if(W<=0){
-				cout << "w should be larger than 0" << endl;
-				quit();
-			}
-			i++;
-		}
-		else{
-			filename=argv[i++];
-		}
-	}
+	string ifilename, ofilename;
+	ifilename = argv[1];
+	ofilename = argv[2];
 
 	srand(time(0));
 
 	Graph g;
-	string name;
-	name=extractFilename(filename.c_str());
-	g.setFilename(name);
-
 	start=clock();
-	g.readGraph(filename);
+	g.readGraph(ifilename);
 	g.Transform();
 	cout << name << " readGraph is complete." << endl;
 	end=clock();
@@ -70,7 +47,7 @@ int main(int argc, char* argv[]){
 	end=clock();
 	cout << "ReOrdered Time Cost: " << (double)(end-start)/CLOCKS_PER_SEC << endl;
 	cout << "Begin Output the Reordered Graph" << endl;
-	g.PrintReOrderedGraph(order);
+	g.PrintReOrderedGraph(order, ofilename);
 	cout << endl;
 }
 
